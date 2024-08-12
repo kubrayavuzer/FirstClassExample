bc6c57294a31183ce8bc92ac2f7924e478192f4e
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-12 18:03:27 +0300

    Proje dosyası ekle.

---
+++ FirstClassExample/FirstClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClassExample
+{
+    public class Person
+    {
+        //properties
+        public string name;
+        public string surname;
+        public string dateofbirth;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public string Dateofbirth
+        {
+            get
+            {
+                return dateofbirth;
+            }
+            set
+            {
+                dateofbirth = value;
+            }
+        }
+
+        //display information
+        public void PrintInformation()     
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Surname: {surname}");
+            Console.WriteLine($"Date Of Birth: {dateofbirth}");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ FirstClassExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using FirstClassExample;
+using System;
+namespace FirstClass
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Person student1 = new Person();
+            student1.name = "Bengü";
+            student1.surname = "Aşık";
+            student1.dateofbirth = "01.01.1990";
+            Person student2 = new Person();
+            student2.name = "Polina";
+            student2.surname = "Red";
+            student2.dateofbirth = "01.01.1990";
+            
+
+            Person teacher1 = new Person();
+            teacher1.name = "Yıldız";
+            teacher1.surname = "Gök";
+            teacher1.dateofbirth = "01.01.1990";
+            Person teacher2 = new Person();
+            teacher2.name = "Ayçil";
+            teacher2.surname = "Akşin";
+            teacher2.dateofbirth = "01.01.1990";
+
+
+            //see information
+
+            Console.WriteLine("Student information");
+            student1.PrintInformation();
+            student2.PrintInformation();
+
+            Console.WriteLine("Teacher information");
+            teacher1.PrintInformation();
+            teacher2.PrintInformation();
+        }
+
+    }
+}
